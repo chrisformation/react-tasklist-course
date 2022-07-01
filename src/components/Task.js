8c4740f8ebf631fa6b722cdd9c87d8517181f78b
formation <formation@diginamic.fr>
2022-07-01 16:43:04 +0200

    Install Bootstrap 5 node package & React Bootstrap node package

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,7 +1,10 @@
+import {Button} from 'react-bootstrap';
+
 const Task = (props) => {
   return (
       <section>
         <h2>{props.label}</h2>
+        <button className="btn btn-warning">Validate</button>
       </section>
   );
 }
